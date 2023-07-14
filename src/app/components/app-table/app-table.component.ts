9e0e1e699e6b4d1b8ac2fd9f2bd06c1d1022306c
parunkov <parunkov@gmail.com>
2023-07-14 13:17:02 +0500

    filter

--- src/app/components/app-table/app-table.component.ts
+++ src/app/components/app-table/app-table.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { FilterService } from 'src/app/services/filter.service';
 import { ELEMENT_DATA, PeriodicElement } from 'src/data/data';
 
 @Component({
@@ -9,7 +11,19 @@ import { ELEMENT_DATA, PeriodicElement } from 'src/data/data';
 export class AppTableComponent {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   data: PeriodicElement[];
-  constructor() {
+  private subscription: Subscription;
+  constructor(
+    private filterSevice: FilterService
+  ) {
     this.data = ELEMENT_DATA;
+    this.subscription = this.filterSevice.filter$.subscribe((value) => {
+      console.log(value);
+      this.data = ELEMENT_DATA.filter((item) => item.name.toLocaleLowerCase().includes(value.toLocaleLowerCase()));
+    })
+  }
+  ngOnInit() {
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
